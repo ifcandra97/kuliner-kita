7519d75140e1b818321b5ecf0db6f2fa70e7b7d1
Candra <ifcandra97@gmail.com>
2023-04-05 09:18:50 +0700

    Upload Model Kuliner dan Response

---
+++ app/src/main/java/com/candra/kulinerkita/Model/ModelKuliner.java
@@ -0,0 +1,26 @@
+package com.candra.kulinerkita.Model;
+
+public class ModelKuliner
+{
+    private String id, nama, asal, deskripsi_singkat;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getNama()
+    {
+        return nama;
+    }
+
+    public String getAsal()
+    {
+        return asal;
+    }
+
+    public String getDeskripsi_singkat()
+    {
+        return deskripsi_singkat;
+    }
+}
---
+++ app/src/main/java/com/candra/kulinerkita/Model/ModelResponse.java
@@ -0,0 +1,24 @@
+package com.candra.kulinerkita.Model;
+
+import java.util.List;
+
+public class ModelResponse
+{
+    private String kode, pesan;
+    private List<ModelKuliner> data;
+
+    public String getKode()
+    {
+        return kode;
+    }
+
+    public String getPesan()
+    {
+        return pesan;
+    }
+
+    public List<ModelKuliner> getData()
+    {
+        return data;
+    }
+}
