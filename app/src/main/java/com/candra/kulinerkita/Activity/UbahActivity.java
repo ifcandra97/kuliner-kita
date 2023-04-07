8994042b2cc2f6f3d12540b983090d9983e8fbf5
candracandra1525 <ifcandra97@gmail.com>
2023-04-07 19:08:20 +0700

    Upload Tambah Activity, Ubah Activity, Main Activity, List Item Kuliner

---
+++ app/src/main/java/com/candra/kulinerkita/Activity/UbahActivity.java
@@ -0,0 +1,16 @@
+package com.candra.kulinerkita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.candra.kulinerkita.R;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah);
+    }
+}
